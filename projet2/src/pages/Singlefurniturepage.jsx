0bd513039d2cbc1bb7925dd64138347dd401d902
Jean-JacquesValmont <jeanjacquesvalmont@gmail.com>
2023-06-29 11:14:50 +0200

    [UPDATE] link to back in page furnitures

--- projet2/src/pages/Singlefurniturepage.jsx
+++ projet2/src/pages/Singlefurniturepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { NavLink } from 'react-router-dom';
 
 const Singlefurniturepage = () => {
     return (
         <div>
             <Header/>
             <h1>Affichage du meuble sur lequel on a cliqué</h1>
+            <NavLink to='/furnitures'>clique ici pour revenir en arrière</NavLink>
             <Footer/>
         </div>
     );
