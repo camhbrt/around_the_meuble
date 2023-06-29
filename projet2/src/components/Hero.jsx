5a01d012e902098a6e0a4f8a1f22e49f16f4da4f
HanaaBelle <hanaabelhamidi@gmail.com>
2023-06-29 16:07:31 +0200

    Added video title in Hero components

--- projet2/src/components/Hero.jsx
+++ projet2/src/components/Hero.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import videoOpeningFurniture from '../assets/videoOpeningFurniture.mp4';
 
 const Hero = () => {
     return (
-        <div>
-           <h1>
-            Jolie vidéo en fond + phrase accrocheuse
-            </h1> 
+        <div className='w-full h-screen relative'>
+            <video className='w-full h-full object-cover'
+            src={videoOpeningFurniture}
+            autoPlay
+            muted
+            />
+        
+            <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'>
+                <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+                    <h1 className='text-6xl/[40px]'>Bienvenue chez vous</h1>
+                    <section id="section01" class="demo">
+                        <h1>Scroll Down Button #1</h1>
+                        <a href="#section02"><span></span>Scroll</a>
+                    </section>
+                </div>
+            
+            </div>
         </div>
     );
 };
