3efdc2087208e1217cab128e4886ef63dc7398b8
Camille Hebert <camille.hebert22@gmail.com>
2023-06-30 11:16:33 +0200

    [UPDATE] link to single furniture detail on the photo

--- projet2/src/components/Articlecard.jsx
+++ projet2/src/components/Articlecard.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Favbutton from './Favbutton';
 import Addtocartbutton from './Addtocartbutton';
+import { NavLink } from 'react-router-dom';
 
 const Articlecard = () => {
     return (
         <div className='w-64 m-5' >
-            <img src="../../public/pioupiou.jpg" alt="fauteuil pioupiou" className='rounded-3xl' />
+            <NavLink to='/singlefurniture'><img src="../../public/pioupiou.jpg" alt="fauteuil pioupiou" className='rounded-3xl' /></NavLink>
             <div className='mb-5 px-5 p-2 bg-slate-400 rounded-3xl' >
                 <p>nom du meuble</p>
                 <p>prix</p>
