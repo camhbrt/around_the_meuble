0ddd57e1893a7e7be6605f4e89b30bbbd3315ddd
HanaaBelle <hanaabelhamidi@gmail.com>
2023-06-30 10:59:54 +0200

    Added ScrollButton in Hero components but it does't appear

--- projet2/src/components/Hero.jsx
+++ projet2/src/components/Hero.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import videoOpeningFurniture from '../assets/videoOpeningFurniture.mp4';
+import ScrollButton from './ScrollButton';
 
 const Hero = () => {
-    return (
-        <div className='w-full h-screen relative'>
-            <video className='w-full h-full object-cover'
-            src={videoOpeningFurniture}
-            autoPlay
-            muted
-            />
-        
-            <div className='absolute w-full h-full top-0 left-0 bg-gray-900/30'>
-                <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
-                    <h1 className='text-6xl/[40px]'>Bienvenue chez vous</h1>
-                    <section id="section01" class="demo">
-                        <h1>Scroll Down Button #1</h1>
-                        <a href="#section02"><span></span>Scroll</a>
-                    </section>
-                </div>
-            
-            </div>
-        </div>
-    );
+  return (
+    <div className='w-full h-screen relative'>
+      <video className='w-full h-full object-cover' src={videoOpeningFurniture} autoPlay muted />
+      <div className='absolute top-0 left-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
+        <h1 className='text-6xl/[40px]'>Bienvenue chez vous</h1>
+      </div>
+      <div className='absolute bottom-5 right-7 z-50'>
+          <ScrollButton />
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
