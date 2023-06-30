c90e6707a4c4d595b8e761ab3e150772650ee768
HanaaBelle <hanaabelhamidi@gmail.com>
2023-06-30 17:50:51 +0200

    Deleted duplicated line

--- projet2/src/pages/Loginpage.jsx
+++ projet2/src/pages/Loginpage.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Loginform from '../components/Loginform';
-import Loginform from '../components/Loginform';
 import Loginbg from '../assets/Loginbg.jpg'
 import Subscriptionform from '../components/Subscriptionform';
 
