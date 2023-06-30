0b65148ca56b00d1ed11fb007d53244b08ca0df5
HanaaBelle <hanaabelhamidi@gmail.com>
2023-06-30 16:34:30 +0200

    Added Subscriptionform component in Loginpage

--- projet2/src/components/Loginform.jsx
+++ projet2/src/components/Loginform.jsx
@@ -37,5 +37,3 @@ const Loginform = () => {
 };
 
 export default Loginform;
-
-//J'ai vu un tuto hier et ce matin sur react, c'est pas encore ça, de mon coté. J'arrive en début d'apèes midi OK ok
--- projet2/src/pages/Loginpage.jsx
+++ projet2/src/pages/Loginpage.jsx
@@ -2,30 +2,45 @@ import React from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import LoginForm from '../components/Loginform';
+import Loginform from '../components/Loginform';
 import Loginbg from '../assets/Loginbg.jpg'
+import Subsciptionform from '../components/Subscriptionform';
 
 const Loginpage = () => {
     return (
         <div>
             <Header/>
-            <div className="flex flex-col flex-auto w-full h-screen">
-                <div className="h-full">
-                    <div className='grid lg:grid-cols-3 h-full'>
-                        <div className='bg-rose-100 bg-cover bg-no-repeat mt-7' style={{backgroundImage: `url(${Loginbg})`}}>
-                            <div className='col-span-2 flex justify-center item-center'>
-                                <div className='min-w-[450px] px-8'>
-                                    <div className='mb-12'>
-                                        <h2 className='text-3xl font-medium'>Welcome Back!</h2>
-                                        <p>Please enter your credentials to Sign In</p>
-                                    </div>
-                                    <LoginForm />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <div className="flex">
+            <div className="w-1/2">
+  <div
+    className="bg-rose-100">
+    <div className="h-screen flex justify-center items-center">
+      <div className="min-w-[450px] px-8">
+        <div className="mb-12">
+          <h2 className="text-3xl font-medium">Register</h2>
+          <p>Create your account. It's free and only take a minute</p>
+        </div>
+        <Subscriptionform />
+      </div>
+    </div>
+  </div>
+</div>
+
+  <div className="w-1/2">
+    <div className="h-screen flex justify-center items-center">
+    <div className="bg-rose-100 bg-cover bg-no-repeat mt-7" style={{backgroundImage: `url(${Loginbg})`}}>
+      <div className="min-w-[450px] px-8">
+        <div className="mb-12">
+          <h2 className="text-3xl font-medium">Welcome Back!</h2>
+          <p>Please enter your credentials to Sign In</p>
+        </div>
+        <Loginform />
+      </div>
+    </div>
+    </div>
+  </div>
+</div>
+
             <Footer/>
         </div>
     );
