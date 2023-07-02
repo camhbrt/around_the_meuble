48317f1e2caff0982e19554deac77bf776393169
HanaaBelle <hanaabelhamidi@gmail.com>
2023-07-02 21:55:20 +0200

    Refactored Favbutton & Addtocartbutton

--- projet2/src/components/Addtocartbutton.jsx
+++ projet2/src/components/Addtocartbutton.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Addtocartbutton = () => {
     return (
-        <div>
-            <p>bouton ajouter au panier</p>
+        <div className="m-3">
+            <button className='block bg-gray-100 hover:bg-gray-200 text-white w-full py-2 px-4 rounded-3xl'>
+            <span className='text-blue-700 cursor-pointer'>
+            Ajouter ✅
+            </span>
+                
+            </button>
         </div>
     );
 };
