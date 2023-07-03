a01a6a65c479031b2f52488607f58adb5b80394d
Camille Hebert <camille.hebert22@gmail.com>
2023-07-03 16:25:11 +0200

    created function to find meuble by ID + created table userfav

---
+++ around_the_meuble/ATM_back/app/models/userfav.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, Sequelize) => {
+    const UserFav = sequelize.define("userfav", {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+
+        user_id: {
+            type: Sequelize.INTEGER,
+            references : {
+                model: 'users',
+                key: 'id'
+            }
+        },
+
+        meuble_id: {
+            type: Sequelize.INTEGER,
+            references : {
+                model: 'meubles',
+                key: 'id'
+            }
+        }
+
+    })
+}
