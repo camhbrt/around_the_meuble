1783396cc7f834177dfd91f45a6da7ab3f979e3e
Clément Badin <clement@MacBook-Air-de-Clement.local>
2023-07-04 12:32:04 +0200

    [CREATE] route & controller back for findAllMeubles [UPDATE] component TableauAdmin

--- around_the_meuble/ATM_back/app/routes/meubles.route.js
+++ around_the_meuble/ATM_back/app/routes/meubles.route.js
@@ -1,10 +1,10 @@
-module.exports = app => {
-    const meubles = require("../controllers/meubles.controller.js");
+module.exports = (app) => {
+  const meubles = require("../controllers/meubles.controller.js");
 
-    let router = require("express").Router();
+  let router = require("express").Router();
 
-    // Une route pour créer un nouveau meuble 
-    router.post("/", meubles.create);
+  // Une route pour créer un nouveau meuble
+  router.post("/", meubles.create);
 
     // Une route pour obtenir toutes les données de tous les meubles
     router.get("/", meubles.findAll);
