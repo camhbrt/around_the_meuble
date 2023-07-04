c781ec0fb9b5fe4f19a8150c80e60e8dd7e974e0
Clément Badin <clement@MacBook-Air-de-Clement.local>
2023-07-04 12:14:50 +0200

    [CREATE] Component to create table for admi

--- around_the_meuble/ATM_back/app/models/index.js
+++ around_the_meuble/ATM_back/app/models/index.js
@@ -2,16 +2,18 @@ const dbConfig = require("../db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorsAliases: false,
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorsAliases: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
+  port: "3306",
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
 });
 
 const db = {};
@@ -19,8 +21,7 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.users= require("./users.model.js")(sequelize, Sequelize);
-db.meubles= require("./meubles.model.js")(sequelize, Sequelize);
-
+db.users = require("./users.model.js")(sequelize, Sequelize);
+db.meubles = require("./meubles.model.js")(sequelize, Sequelize);
 
 module.exports = db;
