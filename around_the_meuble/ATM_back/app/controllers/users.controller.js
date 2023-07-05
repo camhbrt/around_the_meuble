9b534bd3da41180e1925669c6efc6057b5eb085e
HanaaBelle <hanaabelhamidi@gmail.com>
2023-07-05 15:44:34 +0200

    Added password hashing & get token

--- around_the_meuble/ATM_back/app/controllers/users.controller.js
+++ around_the_meuble/ATM_back/app/controllers/users.controller.js
@@ -1,7 +1,22 @@
 const db = require("../models");
 const Users = db.users;
 
-exports.create = (req, res) => {
+ // import du module bcrypt (bibliothèque de hachage de mot de passe pour les crypter)
+ const bcrypt = require('bcrypt');
+
+ // import du module jsonwebtoken utilisé pour créer des tokens d'authentification : JWT (JSON Web Token)
+ const jwt = require('jsonwebtoken');
+
+const hashPassword = async (password) => 
+{await bcrypt.hash(password, 10)}
+
+const findUser = async (user) => 
+{await Users.findOne({where: {email: user}})}
+
+const comparePassword = async (password, hashPassword) => 
+{await bcrypt.compare(password, hashPassword)}
+
+ exports.create = (req, res) => {
   if (!req.body.email) {
     res.status(400).send({
       message: "Entrez un Email",
@@ -10,9 +25,13 @@ exports.create = (req, res) => {
   }
   //Creer un USER dans la base de données
 
+  // bcrypt hash le password du "user".
+  // Il doit faire 10 tours d'execution (d'itérations) pour hasher le password définit comme le "salt"
+  const hashedPassword = hashPassword(req.body.password);
+
   const user = {
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
     nom: req.body.nom,
     prenom: req.body.prenom,
   };
@@ -28,3 +47,32 @@ exports.create = (req, res) => {
       });
     });
 };
+
+exports.getToken = (req, res) => {
+  try {
+    // On cherche dans la BDD un "user" avec l'e-mail fourni.
+    const user = findUser(req.body.email);
+    // Vérification de l'existence du "user", sinon envoie du message 'Authentication failed!'
+    if (!user) {
+        return res.status(401).json({message: 'Authentication failed!'});
+    }
+
+    // bcrypt compare le password fourni avec le password hashé, stocké dans la BDD
+    const isEqual = comparePassword(req.body.password, user.password);
+    // Vérification si les passwords se correspondent, sinon envoie du message 'Authentication failed!'
+    if (!isEqual) {
+        return res.status(401).json({message: 'Authentication failed!'});
+    }
+    // Si les passwords se correspondent, on crée un token d'authentification "JWT"
+    const token = jwt.sign(
+        {userId: user.id, email: user.email},
+        'Super_Secret_Key',
+        {expiresIn: '1h'}
+    );
+    // On envoie une réponse au client avec le token "JWT" et l'ID du "user"
+    res.json({token: token, userId: user.id});
+} catch (error) {
+    // Sinon on affiche l'erreur !
+    next(error);
+}
+}
--- around_the_meuble/ATM_back/app/routes/users.route.js
+++ around_the_meuble/ATM_back/app/routes/users.route.js
@@ -3,8 +3,11 @@ module.exports = app => {
 
     let router = require("express").Router();
 
-    // Une route pour créer un nouveau meuble 
+    // Une route pour créer un nouveau user
     router.post("/", users.create);
+
+    // Une route get pour récupérer un token (pour se connecter)
+    router.get("/login", users.getToken);
 
     // On détermine l'URL pour notre API 
     app.use('/api/users', router);
