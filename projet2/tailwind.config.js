f82584f1c1895253b3bc9a8b229077cafc67ae8f
Clément Badin <clement@MacBook-Air-de-Clement.local>
2023-07-05 16:43:52 +0200

    [STYLE] css et tailwind sur page Admin, footer, header, compoment TableauAdmin, index.html

--- projet2/tailwind.config.js
+++ projet2/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bleuGris: "rgb(179,188,195)",
+        bleuFond : "rgb(229,231,236)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
