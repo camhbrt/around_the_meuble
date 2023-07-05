6d949dd949246c8ebc3333f4fae68c77b09cd7d7
Benjamin Dromard <benjamin.dromard@gmail.com>
2023-07-05 16:23:10 +0200

    Modified routes and controller for users

--- around_the_meuble/ATM_back/app/routes/users.route.js
+++ around_the_meuble/ATM_back/app/routes/users.route.js
@@ -6,6 +6,7 @@ module.exports = app => {
     // Une route pour créer un nouveau meuble 
     router.post("/", users.create);
 
+    // router.post("/login", users.getToken)
     // On détermine l'URL pour notre API 
     app.use('/api/users', router);
 }
